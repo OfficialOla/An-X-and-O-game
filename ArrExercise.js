94deed2d33263a3de47e323aa770b1ccb7d8921d
OLAYINKA ONIYIDE <99347468+OfficialOla@users.noreply.github.com>
2023-02-19 23:50:24 +0100

    Delete ArrExercise.js

--- ArrExercise.js
+++
@@ -1,6 +0,0 @@
-let arrVehicle = [["Car", 2000], ["Truck", 500], ["Bike", 6500], ["Scooter"]]
-arrVehicle[0].push(["Toyota", "Nissan"])
-arrVehicle[1].push(["Ford"])
-arrVehicle[2].push(["Honda"])
- arrVehicle[3].push(["Null"])
- console.table(arrVehicle)
